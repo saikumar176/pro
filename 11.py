97a9c8662b3070af9de3108815608ec47824f9ad
Sai Kumar <52398706+saikumar176@users.noreply.github.com>
2019-07-06 21:58:20 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,5 +1,5 @@
-ir = int(input())
-if ir % 2 == 0:
-    print(ir * (ir // 2) - (ir // 2))
+irs = int(input())
+if irs % 2 == 0:
+    print(irs * (irs // 2) - (irs // 2))
 else:
-    print(ir * (ir // 2))
+    print(irs * (irs // 2))
